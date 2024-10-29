7cb6e806ee65e0d30150b7ea12e722f8a600e1bd
Harri Kuusinen <2306090@edu.karelia.fi>
2024-10-29 15:45:53 +0200

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using AppLogger;
+
+Console.WriteLine("Hello, World!");
+Logger.Log("Tämä_on_aika_sekavaa_mitä_pitää_tehdä");
